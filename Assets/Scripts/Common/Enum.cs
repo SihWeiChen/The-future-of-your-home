b55fb5cdf52931346ee824912e6ee5d97d0ad8eb
sihweichen <s910941@gmail.com>
2019-01-26 14:07:34 +0800

    change sprite

---
+++ Assets/Scripts/Common/Enum.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Common
+{
+    public class GameSetting
+    {
+        public static int PLAYER_MAXIMUM = 4;
+    }
+
+
+
+    public enum ActorDef
+    {
+        Dad = 0,
+        Mom = 1,
+        Son = 2,
+        Daughter = 3,
+    }
+
+    public class EventObject
+    {
+        public string eventName;
+        public int Quality;
+        public int Affection;
+        public int Money;
+        public List<int> openEvent;
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Controller/SelectActorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Controller
+{
+    public class SelectActorController : MonoBehaviour
+    {
+        int curSpriteIndex = 0;
+        Sprite[] arySourceSprite;
+        Image m_Image;
+
+        public SelectActorManager manager;
+
+        // Update is called once per frame
+        void Update()
+        {
+            ChangeActor(curSpriteIndex);
+        }
+
+        public void ChangeActor(Common.ActorDef actor)
+        {
+            int index = (int)actor;
+            ChangeActor(index);
+        }
+        public void ChangeActor(int index)
+        { 
+            curSpriteIndex = index;
+            m_Image.sprite = arySourceSprite[curSpriteIndex];
+        }
+    }
+}
