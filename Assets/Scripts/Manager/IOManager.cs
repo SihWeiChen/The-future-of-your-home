1e43c1f60f015ccdde05519c48cb3f3515cbf3c4
Paper <destiny5420@gmail.com>
2019-01-26 12:04:12 +0800

    [Update] Player IO Setting.

--- Assets/Scripts/Manager/IOManager.cs
+++ Assets/Scripts/Manager/IOManager.cs
@@ -2,17 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IOManager : MonoBehaviour
+namespace Mamager
 {
-    // Start is called before the first frame update
-    void Start()
+    public class IOManager : MonoBehaviour
     {
-        
-    }
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        void Update()
+        {
+            //if (Input.GetKeyDown(KeyCode.JoystickButton0))
+            //    Debug.Log(string.Format("JoystickButton - 0"));
+
+            //if (Input.GetKeyDown(KeyCode.JoystickButton1))
+            //    Debug.Log(string.Format("JoystickButton - 1"));
+
+            //if (Input.GetKeyDown(KeyCode.JoystickButton2))
+            //    Debug.Log(string.Format("JoystickButton - 2"));
+
+            //if (Input.GetKeyDown(KeyCode.JoystickButton3))
+            //    Debug.Log(string.Format("JoystickButton - 3"));
+
+            //if (Input.GetKeyDown(KeyCode.JoystickButton4))
+                //Debug.Log(string.Format("JoystickButton - 4"));
+
+            if (Input.GetButtonDown("P1_Up"))
+                Debug.Log(string.Format("P1_Up"));
+
+            if (Input.GetButtonDown("P1_Down"))
+                Debug.Log(string.Format("P1_Down"));
+
+            if (Input.GetButtonDown("P1_Left"))
+                Debug.Log(string.Format("P1_Left"));
+
+            if (Input.GetButtonDown("P1_Right"))
+                Debug.Log(string.Format("P1_Right"));
+
+            if (Input.GetButtonDown("P2_Up"))
+                Debug.Log(string.Format("P2_Up"));
+
+            if (Input.GetButtonDown("P2_Down"))
+                Debug.Log(string.Format("P2_Down"));
+
+            if (Input.GetButtonDown("P2_Left"))
+                Debug.Log(string.Format("P2_Left"));
+
+            if (Input.GetButtonDown("P2_Right"))
+                Debug.Log(string.Format("P2_Right"));
+            //float fX11 = Input.GetAxis("Horizontal");
+            //float fY11 = Input.GetAxis("Vertical");
+
+            //Debug.Log(string.Format("X11: {0}", fX11));
+            //Debug.Log(string.Format("Y11: {0}", fY11));
+        }
     }
 }
