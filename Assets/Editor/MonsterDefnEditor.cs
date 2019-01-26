613adc4e7dd53b09c3a2a2546f85597b7a4c9af4
sciayk <sciayk@gmail.com>
2019-01-26 12:55:32 +0800

    第二張表

--- Assets/Editor/MonsterDefnEditor.cs
+++ Assets/Editor/MonsterDefnEditor.cs
@@ -9,6 +9,9 @@ public class MonsterDefnEditor : EditorWindow
     string path = "Assets/Resources/Data/GameEventAssset.asset";
     GameEventList  eventList;
 
+    string path2 = "Assets/Resources/Data/GameChooseAssset.asset";
+    ChooseTableList eventList2;
+
     [MenuItem("Window/Data Defn Editor %#m")]
     static void Init()
     {
@@ -19,19 +22,30 @@ public class MonsterDefnEditor : EditorWindow
     {
         eventList = AssetDatabase.LoadAssetAtPath(path,
                             typeof(GameEventList)) as GameEventList;
+        eventList2 = AssetDatabase.LoadAssetAtPath(path2,
+                          typeof(ChooseTableList)) as ChooseTableList;
     }
 
     void OnGUI()
     {
-        if (GUILayout.Button("Create New List"))
+        if (GUILayout.Button("Create EventTabList"))
         {
             eventList = ScriptableObject.CreateInstance<GameEventList>();
             List<GameEventTable> eventTables = new List<GameEventTable>();
             AssetDatabase.CreateAsset(eventList, path);
             AssetDatabase.SaveAssets();
         }
-
-        if (GUI.changed) EditorUtility.SetDirty(eventList);
+        if (GUILayout.Button("Create Chose"))
+        {
+            eventList2 = ScriptableObject.CreateInstance<ChooseTableList>();
+            List<ChooseTableList> eventTables = new List<ChooseTableList>();
+            AssetDatabase.CreateAsset(eventList2, path2);
+            AssetDatabase.SaveAssets();
+        }
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(eventList);
+            EditorUtility.SetDirty(eventList2);
+        }
     }
 }
-
---
+++ Assets/SourceFiles/ChooseTable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseTableList : ScriptableObject
+{
+    public List<ChooseTable> chooseTableList;
+}
+[System.Serializable]
+public class ChooseTable
+{
+    /// <summary>
+    /// NID
+    /// </summary>
+    public int NID;
+    /// <summary>
+    /// 事件名稱
+    /// </summary>
+    public string ChooseName;
+    /// <summary>
+    /// 改變品質
+    /// </summary>
+    public int ChangeQuality;
+    /// <summary>
+    /// 改變感情
+    /// </summary>
+    public int ChangeFeel;
+    /// <summary>
+    /// 改變金錢
+    /// </summary>
+    public int ChangeMoney;
+}
