c17766acc1b3ef3f68c79a4036510f524a3789a6
燈塔 <air92316@gmail.com>
2019-01-27 09:48:04 +0800

    upupup

--- Assets/Scripts/BG/BGLevelUp.cs
+++ Assets/Scripts/BG/BGLevelUp.cs
@@ -5,39 +5,35 @@ using UnityEngine;
 public class BGLevelUp : MonoBehaviour
 {
 
-    public OBJLevelUp[] LVGameObj;
-    public int lv = 1;
+    //public OBJLevelUp[] LVGameObj;
+    private Animator animator;
     // Start is called before the first frame update
     private void Awake()
     {
-        this.LVGameObj = new OBJLevelUp[this.GetComponentsInChildren<OBJLevelUp>().Length];
-        this.LVGameObj =this.GetComponentsInChildren<OBJLevelUp>();
+        //this.LVGameObj = new OBJLevelUp[this.GetComponentsInChildren<OBJLevelUp>().Length];
+        //this.LVGameObj =this.GetComponentsInChildren<OBJLevelUp>();
+        this.animator = this.GetComponent<Animator>();
         this.LevelUp();
 
-        GameLogic.GetInstance.GetGamePlayerManager().Regist(GamePlayManager.RegistType.BGLevelUpController, this);
+
     }
 
 
     public void LevelUp(int LV = 1)
     {
-        if (LV > this.LVGameObj.Length)
-        {
-            LV = this.LVGameObj.Length;
-        }else if (LV <= 0)
-        {
-            LV = 1;
-        }
 
-        for (int i = 0; i < this.LVGameObj.Length; i++)
-        {
-            if (this.LVGameObj[i].name == string.Format("LV{0}", LV))
-            {
-                this.LVGameObj[i].gameObject.SetActive(true);
-            }
-            else
-            {
-                this.LVGameObj[i].gameObject.SetActive(false) ;
-            }
-        }
+        this.animator.SetInteger("LV", LV);
+
+        //for (int i = 0; i < this.LVGameObj.Length; i++)
+        //{
+        //    if (this.LVGameObj[i].name == string.Format("LV{0}", LV))
+        //    {
+        //        this.LVGameObj[i].gameObject.SetActive(true);
+        //    }
+        //    else
+        //    {
+        //        this.LVGameObj[i].gameObject.SetActive(false) ;
+        //    }
+        //}
     }
 }
