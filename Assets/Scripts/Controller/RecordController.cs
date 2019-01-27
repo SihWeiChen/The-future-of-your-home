be666c014232671155ee07f0d901389285fddbb4
Paper <destiny5420@gmail.com>
2019-01-27 11:19:02 +0800

    update

--- Assets/Scripts/Controller/RecordController.cs
+++ Assets/Scripts/Controller/RecordController.cs
@@ -9,19 +9,57 @@ namespace Controller
     {
         [SerializeField] RawImage m_imgPicture;
         [SerializeField] Image m_imgBG;
+        [SerializeField] Animation m_anim;
+
+        bool m_bRoting;
+        float m_fRotValue;
+        const float m_fRotTime = 2.0f;
 
         void Start()
         {
-            
+            m_imgPicture.enabled = false;
+            m_imgBG.enabled = false;
+            m_bRoting = false;
         }
 
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                Texture2D picture = CameraHelper.CaptureScreenshot2(new Rect() { width = 2850.0f, height = 1500.0f });
-                m_imgPicture.texture = picture;
+                ShowRecord();
             }
+
+            if (m_bRoting)
+            {
+                m_fRotValue += Time.deltaTime / m_fRotTime;
+                m_imgPicture.color = new Color(1.0f, 1.0f, 1.0f, m_fRotValue);
+
+                if (m_fRotValue >= 1.0f)
+                {
+                    m_bRoting = false;
+                    m_fRotValue = 1.0f;
+                    m_anim.Stop();
+                    m_imgPicture.color = new Color(1.0f, 1.0f, 1.0f, m_fRotValue);
+                }
+            }
+        }
+
+        public void ShowRecord()
+        {
+            ScreenShot();
+            m_imgPicture.enabled = true;
+            m_imgBG.enabled = true;
+            m_anim.Play();
+            m_bRoting = true;
+
+            m_fRotValue = 0.0f;
+            m_imgPicture.color = new Color(1.0f, 1.0f, 1.0f, m_fRotValue);
+        }
+
+        void ScreenShot()
+        {
+            Texture2D picture = CameraHelper.CaptureScreenshot2(new Rect() { width = 2850.0f, height = 1500.0f });
+            m_imgPicture.texture = picture;
         }
     }
 }
