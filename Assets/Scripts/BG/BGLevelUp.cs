564d911bb17eb22a8278667cdb243fec7278aae9
燈塔 <air92316@gmail.com>
2019-01-27 08:43:57 +0800

    升級!!!!!

---
+++ Assets/Scripts/BG/BGLevelUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGLevelUp : MonoBehaviour
+{
+
+    public OBJLevelUp[] LVGameObj;
+    public int lv = 1;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        this.LVGameObj = new OBJLevelUp[this.GetComponentsInChildren<OBJLevelUp>().Length];
+        this.LVGameObj =this.GetComponentsInChildren<OBJLevelUp>();
+        this.LevelUp();
+    }
+
+
+    public void LevelUp(int LV = 1)
+    {
+        if (LV > this.LVGameObj.Length)
+        {
+            LV = this.LVGameObj.Length;
+        }else if (LV <= 0)
+        {
+            LV = 1;
+        }
+
+        for (int i = 0; i < this.LVGameObj.Length; i++)
+        {
+            if (this.LVGameObj[i].name == string.Format("LV{0}", LV))
+            {
+                this.LVGameObj[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                this.LVGameObj[i].gameObject.SetActive(false) ;
+            }
+        }
+    }
+}
